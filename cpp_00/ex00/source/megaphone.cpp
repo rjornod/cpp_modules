1bf9d61329e9e4b99c4083b37672e435b9f02917
Roberto Jornod <rojornod@f1r2s19.codam.nl>
2025-07-31 17:17:52 +0200

    finished ex00, started ex01

---
+++ cpp_00/ex00/source/megaphone.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	int		i, x;
+	char	ch;
+	if (argc == 1)	
+		std::cout << "LOUD AND UNBEARABLE FEEDBACK NOISE\n"; 
+	else	
+	{
+		for (i = 1; i < argc; i++) 
+		{
+			int len = strlen(argv[i]);
+			for (x = 0; x < len; x++)
+			{
+				ch = toupper(argv[i][x]);
+				std::cout << ch;
+			}
+			if (i < argc - 1)
+				std::cout << " ";
+		}
+	}
+	std::cout << "\n"; 
+	return (0);
+}
