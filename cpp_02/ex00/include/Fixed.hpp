427d19d4db0114819a0f8f561ffe0add923d909e
Roberto Jornod <rojornod@f0r3s13.codam.nl>
2025-10-22 16:39:14 +0200

    made some progress with ex00

--- cpp_02/ex00/include/Fixed.hpp
+++ cpp_02/ex00/include/Fixed.hpp
@@ -1,8 +1,13 @@
 
 class Fixed {
-	public:
+	private:
 		int	_value;
 		static const int _fract_bits = 8;
-	private:
-		Fixed();
+	public:
+		int value;
+		int age;
+		Fixed() : _value(0){}; //default constructor. initializes _value to 0
+		Fixed(const Fixed& copy){}; //copy constructor
+		Fixed &operator=(const Fixed& overload){};
+		~Fixed(void){};
 };
