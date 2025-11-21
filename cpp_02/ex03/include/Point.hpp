2d1148a2d21de88319545788d38f7865cc741c40
Roberto Jornod <rojornod@f1r6s5.codam.nl>
2025-11-21 16:28:21 +0100

    removed implementation for assignment operator for ex03

--- cpp_02/ex03/include/Point.hpp
+++ cpp_02/ex03/include/Point.hpp
@@ -7,9 +7,9 @@ class Point
 		const Fixed _y;
 	public:
 		Point();                                                                                                                                                                                                                                                                                                                                                                                                                              
-		Point( const float n1, const float n2 );
-		Point ( const Point& copy );
-		Point &operator=( const Point& overload ); //because Point store coordinates as const Fixed members, doing so after construction is not possible. delete tells the compiler this assigment is disabled a = b is not possible and will fail at compile time 
+		Point(const float n1, const float n2);
+		Point (const Point& copy);
+		Point &operator=(const Point& overload) = delete; //because Point store coordinates as const Fixed members, doing so after construction is not possible. delete tells the compiler this assigment is disabled a = b is not possible and will fail at compile time 
 		~Point();
 		const Fixed& getXVal() const;
 		const Fixed& getYVal() const;
--- cpp_02/ex03/source/Point.cpp
+++ cpp_02/ex03/source/Point.cpp
@@ -5,7 +5,7 @@ Point::Point() : _x(0), _y(0) {}
 Point::Point( const float n1, const float n2 ) : _x(n1), _y(n2) {}
 
 Point::Point( const Point& copy ):_x(copy._x), _y(copy._y) {}
-Point &Point::operator=(const Point& other){}
+
 Point::~Point() {}
 
 const Fixed& Point::getYVal() const
