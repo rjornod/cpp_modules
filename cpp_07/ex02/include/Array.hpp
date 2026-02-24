d141617aa16a8eae835a685c0afd677618c4f7bd
Roberto Jornod <rojornod@f0r2s7.codam.nl>
2026-02-24 16:26:02 +0100

    added some tests to ex02, cleaned up the output a bit

--- cpp_07/ex02/include/Array.hpp
+++ cpp_07/ex02/include/Array.hpp
@@ -1,5 +1,15 @@
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
+#define RED "\033[1;31m"
+#define WHITE "\033[0m"
+#define BLUE "\033[34m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUEBG "\033[44m"
+#define BLACKBG "\033[0m"
+#define REDBG "\033[40m"
+#define GREENBG "\033[42m"
+
 
 # include <iostream>
 
@@ -8,26 +18,21 @@ class Array
 {
 	private:
 		T 	*_array;
-		int	_size;
+		unsigned int	_size;
 
 	public:
 
     	Array() : _array(nullptr), _size(0) {};
 
-		Array(unsigned int n) : _array(new T[n]()), _size(n) {};
-    
+		Array(unsigned int n) : _array(new T[n]()), _size(n) {};			// the '()' in 'new T[n]()' initializes the values in the array to 0 for built in types (char, int, etc)
+
     	Array(const Array& other) : _array(nullptr), _size(other._size)
 		{
-			std::cout << "startiing to copy..." << std::endl;
 			if (_size > 0)
 			{
 				this->_array = new T[_size];
-				for (int i = 0; i < _size; i++)
-				{
-					std::cout << "copying..." << std::endl;
+				for (unsigned int i = 0; i < _size; i++)
 					this->_array[i] = other._array[i];
-					std::cout << "copied..." << std::endl;
-				}
 			}
 		}
 
@@ -54,11 +59,19 @@ class Array
 			delete[] _array;
 		};
 
-		 int size() const
+		unsigned int size() const
  		{
 			return (_size);
 		}
-		T& operator[](int index)
+
+		T& operator[](unsigned int index)											// subscript operator overload, checks the the index and if its out of bounds it throws an exception
+		{
+			if (index >= _size || index < 0)
+				throw std::out_of_range("Index out of bounds");
+			return (_array[index]);
+		};
+
+		const T& operator[](unsigned int index)	const								// const subscript operator overload, checks the the index and if its out of bounds it throws an exception
 		{
 			if (index >= _size || index < 0)
 				throw std::out_of_range("Index out of bounds");
--- cpp_07/ex02/source/main.cpp
+++ cpp_07/ex02/source/main.cpp
@@ -4,140 +4,84 @@
 #define MAX_VAL 5
 int main(int, char**)
 {
-	// Array<int> test(2);
-	// std::cout << test.size() <<std::endl;
+    Array<int> numbers(MAX_VAL);		// class template, allocates in stack and heap, destructor handles delete
+	//int* mirror = new int[MAX_VAL];	// dynamic array, allocated in heap, requires delete []
+	//const Array<int> test(7);			// const class template
 
-	Array<int> intarray(4);
-	std::cout  <<" size of array is: " << intarray.size() <<std::endl;
-	for (int i = 0; i < intarray.size(); i++)
-    {
-        const int value = rand() % 100;
-        intarray[i] = value;
- 
-		std::cout << intarray[i] << std::endl;
-	}
 
-	std::cout << "changing intarray[1] to 101...." << std::endl;
-	for (int i = 0; i < intarray.size(); i++)
-    {
-        // const int value = rand() % 100;
-        // intarray[i] = value;
- 
-		std::cout << intarray[i] << std::endl;
-	}
-	
-	std::cout << "creating new array ...." << std::endl;
-	Array<int> newarray(intarray);
-	for (int i = 0; i < newarray.size(); i++)
-    {
-        // const int value = rand() % 100;
-        // intarray[i] = value;
- 
-		std::cout << newarray[i] << std::endl;
-	}
-	intarray[1] = 101;
-	std::cout << "checking original array" <<std::endl;
-	for (int i = 0; i < intarray.size(); i++)
-    {
-        // const int value = rand() % 100;
-        // intarray[i] = value;
- 
-		std::cout << intarray[i] << std::endl;
-	}
-	std::cout << "checking new array" <<std::endl;
-	for (int i = 0; i < newarray.size(); i++)
-    {
-        // const int value = rand() % 100;
-        // intarray[i] = value;
- 
-		std::cout << newarray[i] << std::endl;
-	}
-	intarray = newarray;
-	std::cout << "checking original array after copy assignment" <<std::endl;
-	for (int i = 0; i < intarray.size(); i++)
-    {
-        // const int value = rand() % 100;
-        // intarray[i] = value;
- 
-		std::cout << intarray[i] << std::endl;
-	}
-	std::cout << "checking new array after copy assignment" <<std::endl;
-	for (int i = 0; i < newarray.size(); i++)
-    {
-        // const int value = rand() % 100;
-        // intarray[i] = value;
- 
-		std::cout << newarray[i] << std::endl;
-	}
-	intarray[1] = 101;
-	std::cout << "checking original array after modifying 1" <<std::endl;
-	for (int i = 0; i < intarray.size(); i++)
-    {
-        // const int value = rand() % 100;
-        // intarray[i] = value;
- 
-		std::cout << intarray[i] << std::endl;
-	}
-	std::cout << "checking new array after moddifying 1" <<std::endl;
-	for (int i = 0; i < newarray.size(); i++)
-    {
-        // const int value = rand() % 100;
-        // intarray[i] = value;
- 
-		std::cout << newarray[i] << std::endl;
-	}
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-	int * a = new int[5];
-	std::cout << "*a = " << *a <<std::endl;
-    srand(time(NULL));
+    std::cout << GREEN << "Creating a class template NUMBERS array with size " << numbers.size() << WHITE <<std::endl;
+	srand(time(NULL));
+	std::cout << BLUE << "\n.... Assigning the random values to NUMBERS ....\n" << BLUE << std::endl;
     for (int i = 0; i < MAX_VAL; i++)
     {
-        const int value = rand() % 100;
+        const int value = rand();
         numbers[i] = value;
-        mirror[i] = value;
-		std::cout << numbers[i] << std::endl;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
     }
 
-    for (int i = 0; i < MAX_VAL; i++)
+	Array<int> characters(MAX_VAL);
+	for (int i = 0; i < MAX_VAL; i++)
     {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-		else
-			std::cout << "Both elements are equal" << std::endl;
+		static int chara = 97;
+        characters[i] = chara++;
     }
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		std::cout << GREEN << "index [" << i << "]\n" << WHITE << "char    = " << static_cast<char>(characters[i]) << std::endl;
+	}
+
+	std::cout << BLUE << "\nCOPY ASSIGNMENT TEST numberscpy(numbers)\n" << WHITE << std::endl;
+	Array<int> numberscpy(numbers);
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		std::cout << GREEN << "index [" << i << "]\n" << WHITE << "numbers    = " << numbers[i] << "\nnumberscpy = " << numberscpy[i] << "\n" << std::endl;
+	}
+
+/************************************************************************************************************************/
+	
+	std::cout << BLUE << "DEEP COPY CHECK" << WHITE << std::endl;
+	for (int i = 0; i < MAX_VAL; i++)
+		numbers[i] = 1;
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		std::cout << GREEN << "index [" << i << "]\n" << WHITE << "numbers    = " << numbers[i] << "\nnumberscpy = " << numberscpy[i] << "\n" << std::endl;
+	}
+
+/***********************************************************************************************************************/
+	
+	std::cout << RED << "FAILURE CHECKS\n" << WHITE << std::endl;
+	std::cout << BLUEBG << " Checking for negative index (-2) on NUMBERS " << BLACKBG << std::endl;
     try
     {
         numbers[-2] = 0;
     }
     catch(const std::exception& e)
     {
-        std::cerr << e.what() << '\n';
+        std::cerr << RED << "Error: " << WHITE << e.what() << '\n';
     }
+	std::cout << "\n" << BLUEBG << " Checking for index above MAX_VAL NUMBERS " << BLACKBG << std::endl;
     try
     {
         numbers[MAX_VAL] = 0;
     }
     catch(const std::exception& e)
     {
-        std::cerr << e.what() << '\n';
+        std::cerr <<  RED << "Error: " << WHITE << e.what() << '\n';
     }
+	try
+	{
+
+		std::cout << "\n" << BLUEBG << " Accessing an empty array " << BLACKBG << std::endl;
+		Array<char> emptychar;
+		std::cout << emptychar[0];
+	}
+	catch (const std::exception& e)
+	{
+        std::cerr <<  RED << "Error: " << WHITE << e.what() << '\n';
+	}
 
     for (int i = 0; i < MAX_VAL; i++)
     {
         numbers[i] = rand();
     }
-    delete [] mirror;//
-	delete [] a;
-	// delete [] intarray;
-	// delete [] newarray;
     return 0;
 }
