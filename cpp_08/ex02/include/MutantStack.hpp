36a45f088f31038901ee87bc99fc65ac1d1025bc
rjornod <110393443+rjornod@users.noreply.github.com>
2026-03-05 13:51:34 +0100

    added MutantStack tests and updated Makefile

--- cpp_08/ex02/include/MutantStack.hpp
+++ cpp_08/ex02/include/MutantStack.hpp
@@ -10,10 +10,13 @@ class MutantStack : public std::stack<T>
 	public:
 		MutantStack(){};
 		~MutantStack(){};
-		MutantStack(const MutantStack& other){};
-		MutantStack& operator=(const MutantStack& other){};
-		typename std::stack<T>::container_type::iterator begin() {return this->c.begin();}
-		typename std::stack<T>::container_type::iterator end() {return this->c.end();}
+		// MutantStack(const MutantStack& other){};
+		// MutantStack& operator=(const MutantStack& other){};
+
+		// typename is required in the return type becasue T is unknown at compile time and the compiler needs that to know that ::iterator refers to a type, not a static variable
+		typename std::stack<T>::container_type::iterator begin() {return this->c.begin();}	// returns an iterator to the first element. exposed from std::deque which stack is based on
+		typename std::stack<T>::container_type::iterator end() {return this->c.end();} 		// returns an iterator to the last element. also exposed from std::deque.
+		using iterator = typename std::stack<T>::container_type::iterator;					// creates an alias from the typename above
 	private:
 };
 
--- cpp_08/ex02/source/main.cpp
+++ cpp_08/ex02/source/main.cpp
@@ -1,11 +1,55 @@
 #include <MutantStack.hpp>
-
+#include <list>
 int	main(void)
 {
-	MutantStack<int> test;
+	std::cout << "MutantStack test" << std::endl;
+	// MUTANT STACK TEST
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+	std::cout << *it << std::endl;
+	++it;
+	}
+	std::stack<int> s(mstack);
+	// return 0;
 
-	
-	
+	std::cout << "std::list test" << std::endl;
 
-
+	//STD LIST TEST
+	std::list<int> mlist;
+	mlist.push_back(5);
+	mlist.push_back(17);
+	std::cout << mlist.back() << std::endl;
+	mlist.pop_back();
+	std::cout << mlist.size() << std::endl;
+	mlist.push_back(3);
+	mlist.push_back(5);
+	mlist.push_back(737);
+	//[...]
+	mlist.push_back(0);
+	std::list<int>::iterator itlist = mlist.begin();
+	std::list<int>::iterator itelist = mlist.end();
+	++itlist;
+	--itlist;
+	while (itlist != itelist)
+	{
+	std::cout << *itlist << std::endl;
+	++itlist;
+	}
+	std::list<int> l(mlist);
+	return 0;
 }
