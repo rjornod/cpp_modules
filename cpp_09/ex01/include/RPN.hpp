972ede2e543cf5c5ab22b9fe388b3c5d83faf060
rjornod <110393443+rjornod@users.noreply.github.com>
2026-08-25 15:04:24 +0200

    finished full implementation of rpn

--- cpp_09/ex01/include/RPN.hpp
+++ cpp_09/ex01/include/RPN.hpp
@@ -2,18 +2,28 @@
 #include <stack>
 #include <iostream>
 
+enum OperandType {
+	ADDITION,
+	SUBTRACTION,
+	MULTIPLICATION,
+	DIVISION,
+	UNKNOWN
+};
+
 class Rpn {
 	private:
-		std::stack<std::string> m_stack;
-		// std::string							m_stringToken;
-		// unsigned int						m_valueToken;
+		std::stack<int> m_stack;
+		unsigned int		m_intAmount;
 	public:
-		Rpn();
+		Rpn() = delete;
 		Rpn(const std::string& expression);
-		~Rpn();
+		~Rpn(); 
 		Rpn(const Rpn& other);
 		Rpn operator=(const Rpn& other);
 		void startRPN(std::string expression);
 		bool isTokenValid(const std::string& token);
 		bool isTokenDigit(const std::string& token);
-};
+		bool applyOperation(const std::string& operand);
+		bool getResult(long long num1, long long num2, const std::string& operand);
+		OperandType operandFromString(const std::string& operand);
+	};
--- cpp_09/ex01/source/RPN.cpp
+++ cpp_09/ex01/source/RPN.cpp
@@ -2,62 +2,61 @@
 #include <string>
 #include <limits>
 
-Rpn::Rpn() {
-
+Rpn::Rpn(const std::string& expression) : m_intAmount(0) {
+	startRPN(expression);
 }
 
-Rpn::Rpn(const std::string& expression) {
-  startRPN(expression);
-}
+Rpn::~Rpn() {}
 
-Rpn::~Rpn() {
-
-}
-
-Rpn::Rpn(const Rpn& other) : m_stack(other.m_stack) {
-
-}
+Rpn::Rpn(const Rpn& other) : m_stack(other.m_stack), m_intAmount(m_intAmount) {}
 
 Rpn Rpn::operator=(const Rpn& other) {
   if (this != &other) {
     m_stack = other.m_stack;
+		m_intAmount = other.m_intAmount;
   }
   return *this;
 }
 
 void Rpn::startRPN(std::string expression) {
-  // size_t i = 0;
-  // size_t start = 0;
+  if (expression.empty())
+		return;
   size_t end = 0;
   std::string temp;
   while (!expression.empty()) {
     end = expression.find_first_of(" \t\n\r\f\v");
     temp = expression.substr(0, end);
 		expression.erase(0, expression.find_first_not_of(" \t\n\r\f\v", end));
-		// erase token + any run of trailing delimiters in one go
-		if (!temp.empty() && isTokenValid(temp)) {
-			m_stack.emplace(temp);
-			std::cout << "stack: " << m_stack.top() << "\n";
-			
+		if (isTokenDigit(temp)) {
+			m_intAmount++;
+			int token = std::stoll(temp);
+			m_stack.push(token);
+		}
+		else if (isTokenValid(temp) && m_intAmount >= 2) {
+			if (!applyOperation(temp)) {
+				std::cerr << "Error\n";
+				return;
+			}
 		}
 		else {
 			std::cerr << "Error\n";
-			return ;
+			return;
 		}
-		std::cout << "'" << temp << "'\n";
-		if (end == std::string::npos) {
-			std::cout << "clearing\n";
+		if (expression.empty() && m_intAmount != 1) {
+			std::cerr << "Error\n";
+			return;
+		}
+		if (end == std::string::npos)
       expression.clear();
-    }
   }
+	std::cout << m_stack.top() << std::endl;
 }
 
 bool Rpn::isTokenValid(const std::string& token) {
  return token == "+"  || 
  				token == "-" || 
 				token == "*" ||
-				token == "/" || 
-				isTokenDigit(token);
+				token == "/"; 
 }
 
 bool Rpn::isTokenDigit(const std::string& token) {
@@ -80,4 +79,50 @@ bool Rpn::isTokenDigit(const std::string& token) {
 		return false;
 	}
 	return true;
+}
+
+bool Rpn::applyOperation(const std::string& operand) {
+	long long num1 = m_stack.top();
+	m_stack.pop();
+	long long num2 = m_stack.top();
+	m_stack.pop();
+	if (!getResult(num1, num2, operand)) 
+		return false;
+	return true;
+}
+
+bool Rpn::getResult(long long num1, long long num2, const std::string& operand) {
+	long long result;
+	switch (operandFromString(operand)) {
+		case OperandType::ADDITION:
+			result = num2 + num1;
+			break;
+		case OperandType::SUBTRACTION:
+			result = num2 - num1;
+			break;
+		case OperandType::DIVISION:
+			if (num1 == 0)
+				return false;
+			result = num2 / num1;
+			break;
+		case OperandType::MULTIPLICATION:
+			result = num2 * num1;
+			break;
+		case UNKNOWN:
+			return false;
+
+	}	
+	if (result > 2147483647)
+		return false;
+	m_stack.emplace(result);
+	m_intAmount--;
+	return true;
+}
+
+OperandType Rpn::operandFromString(const std::string& operand) {
+	if (operand == "+") return OperandType::ADDITION;
+	if (operand == "-") return OperandType::SUBTRACTION;
+	if (operand == "*") return OperandType::MULTIPLICATION;
+	if (operand == "/")	return OperandType::DIVISION;
+	else 								return OperandType::UNKNOWN;
 }
